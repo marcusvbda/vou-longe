c3960632c5e6f7cfd09015e40f7c23f604b8beb8
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-10-03 16:11:58 -0300

    corrige sliders

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { use, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AspectRatio from './aspectRatio';
 import { ThemeContext } from '@/app/context/themeContext';
 
@@ -13,9 +13,33 @@ export default function Carousel() {
 	);
 	const [currentSlide, setCurrentSlide] = useState(0);
 
+	// Função para mover para o próximo slide
+	const nextSlide = () => {
+		setCurrentSlide((prevSlide) =>
+			prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+		);
+	};
+
+	// Função para mover para o slide anterior
+	const prevSlide = () => {
+		setCurrentSlide((prevSlide) =>
+			prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+		);
+	};
+
+	// Lógica para mudança automática de slides a cada 5 segundos
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			nextSlide();
+		}, 5000); // Muda automaticamente a cada 5 segundos
+
+		// Cleanup para remover o intervalo quando o componente desmontar
+		return () => clearInterval(intervalId);
+	}, [currentSlide, slides.length]);
+
 	return (
 		<div className="relative w-full h-[634px] mb-8 top-[-130px]">
-			<div className="overflow-hidden h-full n w-full">
+			<div className="overflow-hidden h-full w-full">
 				<div
 					className="flex h-full transition-transform duration-500 ease-in-out"
 					style={{ transform: `translateX(-${currentSlide * 100}%)` }}
@@ -32,11 +56,8 @@ export default function Carousel() {
 						>
 							<div className="w-full flex h-full items-center justify-between">
 								<button
-									onClick={() => setCurrentSlide(Math.max(currentSlide - 1, 0))}
-									disabled={currentSlide === 0}
-									className={`cursor-pointer hover:opacity-70 transition duration-300 ${
-										currentSlide === 0 ? 'opacity-50' : ''
-									}`}
+									onClick={prevSlide}
+									className="cursor-pointer hover:opacity-70 transition duration-300"
 								>
 									<AspectRatio
 										src="/assets/images/arrow-btn-prev.svg"
@@ -45,7 +66,7 @@ export default function Carousel() {
 									/>
 								</button>
 								<div className="w-full flex px-20">
-									<div className="w-full md:w-5/12 flex flex-col">
+									<div className="w-full md:w-7/12 flex flex-col">
 										{slide[1] && (
 											<h1
 												className="text-6xl font-semibold mt-10 md:mt-0 font-lexend-deca"
@@ -61,37 +82,28 @@ export default function Carousel() {
 									</div>
 								</div>
 								<button
-									onClick={() =>
-										setCurrentSlide(
-											Math.min(currentSlide + 1, slides.length - 1)
-										)
-									}
-									disabled={currentSlide === slides.length - 1}
-									className={`cursor-pointer hover:opacity-70 transition duration-300 ${
-										currentSlide === slides.length - 1 ? 'opacity-50' : ''
-									}`}
+									onClick={nextSlide}
+									className="cursor-pointer hover:opacity-70 transition duration-300"
 								>
 									<AspectRatio
 										src="/assets/images/arrow-btn-next.svg"
-										alt="Previous"
+										alt="Next"
 										size={{ width: 50 }}
 									/>
 								</button>
 							</div>
-							<div className="w-100 justify-center bottom-4 mt-auto relative -top-20 flex gap-2">
-								{slides.map((_: any, index: any) => (
-									<button
-										key={index}
-										onClick={() => setCurrentSlide(index)}
-										className={`cursor-pointer bg-white h-4 ${
-											currentSlide === index
-												? 'w-8 rounded-lg'
-												: 'rounded-md w-4'
-										}`}
-									/>
-								))}
-							</div>
 						</div>
+					))}
+				</div>
+				<div className="w-100 transition-all duration-300 justify-center bottom-4 mt-auto relative -top-20 flex gap-2">
+					{slides.map((_: any, index: any) => (
+						<button
+							key={index}
+							onClick={() => setCurrentSlide(index)}
+							className={`cursor-pointer bg-white h-4 ${
+								currentSlide === index ? 'w-8 rounded-lg' : 'rounded-md w-4'
+							}`}
+						/>
 					))}
 				</div>
 			</div>
