9b773c1ca5f2a1cf94a9c1cd59456551995da14f
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-10-08 21:06:14 -0300

    campos agrupados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,22 @@ const HeroContent = ({ site }: any) => {
 	return (
 		<div className="w-full flex flex-col md:flex-row gap-2 px-4 items-center">
 			<div className="w-full md:w-6/12 pr-0 md:pr-10 order-1 md:order-0">
-				<h5 className="text-primary">{site?.acf?.hero_linha_1 || ''}</h5>
+				<h5 className="text-primary">
+					{site?.acf?.banner_1?.hero_linha_1 || ''}
+				</h5>
 				<h1
 					className="text-3xl text-neutral-800 font-bold font-lexend-deca mb-4"
-					dangerouslySetInnerHTML={{ __html: site?.acf?.hero_linha_2 || '' }}
+					dangerouslySetInnerHTML={{
+						__html: site?.acf?.banner_1?.hero_linha_2 || '',
+					}}
 				/>
 				<h3 className="text-sm text-neutral-500">
-					{site?.acf?.hero_linha_3 || ''}
+					{site?.acf?.banner_1?.hero_linha_3 || ''}
 				</h3>
 				<div className="flex flex-col md:flex-row gap-2 px-0 mt-10 w-auto">
 					<div>
 						<Link
-							href={site?.acf?.comece_agora_link || '#'}
+							href={site?.acf?.banner_1?.comece_agora_link || '#'}
 							className="cursor-pointer px-4 bg-primary text-white p-3 rounded-xl w-full flex justify-center items-center text-sm"
 						>
 							Comece agora
@@ -26,7 +30,7 @@ const HeroContent = ({ site }: any) => {
 					</div>
 					<div>
 						<Link
-							href={site?.acf?.saiba_mais_link || '#'}
+							href={site?.acf?.banner_1?.saiba_mais_link || '#'}
 							className="cursor-pointer border px-4 text-primary border-primary p-3 rounded-xl w-full flex justify-center items-center text-sm"
 						>
 							Saiba mais
@@ -35,7 +39,7 @@ const HeroContent = ({ site }: any) => {
 				</div>
 			</div>
 			<div className=" w-full md:w-6/12 order-0 md:order-1">
-				<img src={site?.acf?.hero_imagem || ''} />
+				<img src={site?.acf?.banner_1?.hero_imagem || ''} />
 			</div>
 		</div>
 	);
@@ -43,7 +47,7 @@ const HeroContent = ({ site }: any) => {
 
 export default async function HomePage() {
 	const site = await getSite();
-	// console.log(site?.acf);
+	// console.log(site?.acf?.banner_1?.hero_linha_1);
 
 	return (
 		<>
--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -1,31 +1,30 @@
 import Footer from '@/components/footer';
 import PublicNavbar from '@/components/PublicNavbar';
 import { getSite } from '@/services/auth';
-import Link from 'next/link';
 
 const HeroContent = ({ site }: any) => {
 	return (
 		<div className="w-full flex flex-col gap-2 px-4 items-center mt-20 md:px-32">
 			<div className="w-full">
 				<h1 className="text-neutral-800 font-bold font-lexend-deca text-3xl text-center mb-6">
-					{site?.acf?.hero_sobre_linha_1 || ''}
+					{site?.acf?.banner_sobre_1?.hero_sobre_linha_1 || ''}
 				</h1>
 				<h3 className="text-neutral-500 font-lexend text-center">
-					{site?.acf?.hero_sobre_linha_2 || ''}
+					{site?.acf?.banner_sobre_1?.hero_sobre_linha_2 || ''}
 				</h3>
 			</div>
 			<div className="w-full">
-				<img src={site?.acf?.hero_sobre_linha_url || ''} />
+				<img src={site?.acf?.banner_sobre_1?.hero_sobre_linha_url || ''} />
 			</div>
 			<div className="w-full">
 				<h4 className="text-primary mt-6 font-lexend-deca text-center mb-6">
-					{site?.acf?.hero_sobre_linha_3 || ''}
+					{site?.acf?.banner_sobre_1?.hero_sobre_linha_3 || ''}
 				</h4>
 				<h2 className="text-neutral-800 font-bold font-lexend-deca text-2xl text-center mb-4">
-					{site?.acf?.hero_sobre_linha_4 || ''}
+					{site?.acf?.banner_sobre_1?.hero_sobre_linha_4 || ''}
 				</h2>
 				<h3 className="text-neutral-500 font-lexend text-center">
-					{site?.acf?.hero_sobre_linha_5 || ''}
+					{site?.acf?.banner_sobre_1?.hero_sobre_linha_5 || ''}
 				</h3>
 			</div>
 		</div>
@@ -35,6 +34,7 @@ const HeroContent = ({ site }: any) => {
 export default async function AboutPage() {
 	const site = await getSite();
 	// console.log(site?.acf);
+	// console.log(site?.acf?.banner_sobre_1?.hero_linha_1);
 
 	return (
 		<>
